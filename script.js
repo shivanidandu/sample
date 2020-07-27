4ad6391f3caefa95c2a65ba0b9cdf209aada4198
Dandu shivani <59301837+shivanidandu@users.noreply.github.com>
2020-07-27 18:19:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+function Telugu()
+{
+
+document.getElementById('p1').innerHTML=" షిర్డీ సాయిబాబా";
+
+
+}
+
+function Hindi()
+{
+
+;
+document.getElementById('a1').innerHTML=" साईबाबा";
+
+
+}
+
+function Tamil()
+{
+
+document.getElementById('h1').innerHTML=" சீரடி சாயி பாபா";
+
+}
